d4c673fe39fe84cac98a71211b0171ee7d48d0f8
Mark Goberdhan <marc@Marks-MBP.home>
2017-03-15 02:03:40 -0400

    prep repo for heroku

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 var express = require('express');
 
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+// middleware to change https to http for api
+app.use(function(req, res, next){
+  // check if headers has http req
+  // process req normally if so, else redirect
+  if(req.headers['x-forwarded-proto'] === 'http'){
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 app.use(express.static('public'));
 
-app.listen(3000, function() {
-  console.log('Express server up on port 3000');
+app.listen(PORT, function() {
+  console.log('Express server up on port ' + PORT);
 });
