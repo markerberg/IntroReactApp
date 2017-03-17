28a28326f60a3bac4e3d3b7c9fb6f1eb8213a3a9
Mark Goberdhan <marc@Marks-MBP.home>
2017-03-17 13:33:53 -0400

    Fix Error modal bug

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,4 +1,6 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactDOMServer = require('react-dom/server');
 
 var ErrorModal = React.createClass({
   getDefaultProps: function() {
@@ -11,13 +13,8 @@ var ErrorModal = React.createClass({
     message: React.PropTypes.string.isRequired
   },
   componentDidMount: function () {
-    // new instance of foundation reveal, pass in what we use as modal
-    var modal = new Foundation.Reveal($('#error-modal'));
-    modal.open();
-  },
-  render: function () {
     var {title, message} = this.props;
-    return(
+    var modalMarkup = (
       <div id="error-modal" className="reveal tiny text-center" data-reveal="">
         <h4>{title}</h4>
         <p>{message}</p>
@@ -26,6 +23,24 @@ var ErrorModal = React.createClass({
             Okay
           </button>
         </p>
+      </div>
+    );
+
+    // React doesnt work well with third party lib that alters DOM, so we move it from render: to componentDidMount
+    // take jsx code and create a string version, which we use to create elements to return to screen
+    var $modal = $(ReactDOMServer.renderToString(modalMarkup));
+    // use jquery to search for this current react component
+    // then use jquery html method to pass markup to add(the $modal)
+    $(ReactDOM.findDOMNode(this)).html($modal);
+
+    // new instance of foundation reveal, pass in what we use as modal
+    var modal = new Foundation.Reveal($('#error-modal'));
+    modal.open();
+  },
+  render: function () {
+    // we dont include anything in return becuase we do so in componentDidMount
+    return(
+      <div>
       </div>
     );
   }
