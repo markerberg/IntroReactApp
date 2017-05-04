c50b061b82e324982c4333117da26e3ffeed57bc
Mark Goberdhan <marc@Marks-MBP.home>
2017-05-04 14:12:46 -0400

    configure countdown

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -4,11 +4,36 @@ var CountdownForm = require('CountdownForm');
 
 var Countdown = React.createClass({
   getInitialState: function() {
-    return {count: 0};
+    return {
+      count: 0,
+      countdownStatus: 'stopped'
+    };
+  },
+  // lifecycle that gets called after state or prop get updated
+  componentDidUpdate: function (prevProps, prevState) {
+    // check if new countdownStatus in handleSetCountdown is differnet from initial state
+    if(this.state.countdownStatus !== prevState.countdownStatus) {
+      switch (this.state.countdownStatus) {
+        case 'started':
+          this.startTimer();
+          break;
+      }
+    }
+  },
+  startTimer: function () {
+    this.timer = setInterval(() => { // run func every second
+      var newCount = this.state.count - 1;
+      this.setState({ // decrement timer by 1 second
+        // ternary operator
+        // if newCount>=0 true use newCount value, else false we use 0
+        count: newCount >= 0 ? newCount : 0
+      });
+    }, 1000);
   },
   handleSetCountdown: function (seconds) {
     this.setState({
-      count: seconds
+      count: seconds,
+      countdownStatus: 'started'
     });
   },
   render: function () {
