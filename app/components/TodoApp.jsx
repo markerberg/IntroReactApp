b63cfda31596d61e26e11c7aad16d60177f6de3b
Mark Goberdhan <marc@Marks-MBP.home>
2017-05-25 15:34:58 -0400

    nest todo component

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,31 @@
 var React = require('react');
+var TodoList = require('TodoList');
 
 var TodoApp = React.createClass({
+  getInitialState: function (){
+    return {
+      todos: [
+        {
+          id: 1,
+          text: 'Walk pet'
+        }, {
+          id: 2,
+          text: 'Clean house'
+        }, {
+          id: 3,
+          text: 'Buy food'
+        }, {
+          id: 4,
+          text: 'Wash car'
+        }
+      ]
+    };
+  },
   render: function() {
+    var {todos} = this.state;
     return (
       <div>
-        TodoApp.jsx
+        <TodoList todos={todos}/>
       </div>
     )
   }
