099f57f38a0270334d796a436198610c2822d92b
Mark Goberdhan <marc@Marks-MBP.home>
2017-06-07 00:22:36 -0400

    complete handleAddTodo

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -10,4 +10,15 @@ describe('TodoApp', () => {
   it('should exist', ()=> {
     expect(TodoApp).toExist();
   });
+
+  it('should add todo to todos state on handleAddTodo', () => {
+    var todoText = 'dummy test text';
+    var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+
+    todoApp.setState({todos: []}); // make state empty array
+    todoApp.handleAddTodo(todoText); // call function and pass dummy text
+
+    //assertions to see if new state is same as text
+    expect(todoApp.state.todos[0].text).toBe(todoText);
+  });
 });
